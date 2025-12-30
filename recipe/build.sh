d873db4e98d1a66efc6c68568ce48ce2d93fc6f5
j34ni <jeani@uio.no>
2025-12-30 19:12:17 +0000

    Add DCMAKE_POLICY_VERSION_MINIMUM=3.5

--- recipe/build.sh
+++ recipe/build.sh
@@ -39,6 +39,7 @@ export FC=mpifort
 mkdir build && cd build
 cmake ${CMAKE_ARGS} \
     $EXTRA_CMAKE \
+    -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
     -DBLAS_LIBRARIES="blas" \
     -DLAPACK_LIBRARIES="lapack" \
     -DBUILD_SHARED_LIBS=ON \
